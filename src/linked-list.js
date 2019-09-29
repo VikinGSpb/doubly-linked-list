438a3eb00952a406ccf458b3ea38358da8e2850b
VikinGSpb <imbot@inbox.ru>
2019-09-29 14:52:49 +0300

    feat: add at insertAt isEmpty clear

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,13 +4,14 @@ class LinkedList {
     constructor() {
         this.dataStorage = [];
         this.length = 0;
-        this._head = this.dataStorage[0];
-        this._tail = this.dataStorage[0];
+        this._head = 0;
+        this._tail = 0;
     }
 
     append(data) {
         this.dataStorage.push(data);
         this.length++;
+        this._head = this.dataStorage[0];
         this._tail = this.dataStorage[this.length - 1];
     }
 
@@ -22,13 +23,24 @@ class LinkedList {
         return this.dataStorage[this.length - 1];
     }
 
-    at(index) {}
+    at(index) {
+        return this.dataStorage[index];
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        this.dataStorage.splice(index, 0, data);
+    }
 
-    isEmpty() {}
+    isEmpty() {
+        return this.length == 0;
+    }
 
-    clear() {}
+    clear() {
+        this.dataStorage.splice(0, this.length - 1);
+        this.length = 0;
+        this._tail = 0;
+        this._head = 0;
+    }
 
     deleteAt(index) {}
 
